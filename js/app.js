e254d2fa1790fb902b7fec4fe7c0b4007756b426
siamcse <siamcse01@gmail.com>
2022-08-30 20:58:18 +0600

    image size changed

--- js/app.js
+++ js/app.js
@@ -30,8 +30,8 @@ const displayPhones = (phones, dataLimit) =>{
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
-            <div class="card h-100 p-4">
-                <img src="${image}" class="card-img-top" alt="...">
+            <div class="card p-4">
+                <img src="${image}" class="card-img-top w-50" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${phone_name}</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
